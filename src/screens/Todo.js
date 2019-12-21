8f73e6355b417d99d1239fc1181d4f234b4a68fb
Darul Khair <arulkhair@gmail.com>
2019-12-21 18:19:23 +0800

    update todo

--- src/screens/Todo.js
+++ src/screens/Todo.js
@@ -14,6 +14,9 @@ import axios from 'axios';
 import List from './List';
 import {ScrollView} from 'react-native-gesture-handler';
 import {StyleSheet, ToastAndroid} from 'react-native';
+import uuidv1 from 'uuid/v1';
+import AsyncStorage from '@react-native-community/async-storage';
+
 class Todo extends Component {
   constructor(props) {
     super(props);
@@ -22,77 +25,106 @@ class Todo extends Component {
       newtodo: '',
     };
   }
+
   componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5').then(res =>
-      this.setState({
-        todos: res.data,
-      }),
-    );
+    this.loadTodos();
   }
 
-  //Checklist complete
-  markComplete = id => {
-    // console.log(id);
-    this.setState({
-      todos: this.state.todos.map(todo => {
-        if (todo.id === id) {
-          todo.completed = !todo.completed;
-        }
-        return todo;
-      }),
+  loadTodos = async () => {
+    try {
+      const getTodos = await AsyncStorage.getItem('todos');
+      const parsedTodos = JSON.parse(getTodos);
+      this.setState({todos: parsedTodos || {}});
+    } catch (err) {
+      console.log(err);
+    }
+    // console.log(getTodos);
+  };
+
+  saveTodos = newToDos => {
+    const saveTodos = AsyncStorage.setItem('todos', JSON.stringify(newToDos));
+    // this.loadTodos();
+  };
+
+  addTodo = title => {
+    this.setState({newtodo: ''});
+    const {newTodoItem} = title;
+
+    if (newTodoItem !== '') {
+      this.setState(prevState => {
+        const ID = uuidv1();
+        const newToDoObject = {
+          [ID]: {
+            userId: 1,
+            id: ID,
+            title: title,
+            completed: false,
+          },
+        };
+        const newState = {
+          ...prevState,
+          newTodoItem: '',
+          todos: {
+            ...prevState.todos,
+            ...newToDoObject,
+          },
+        };
+        this.saveTodos(newState.todos); // add this
+        return {...newState};
+      });
+      ToastAndroid.showWithGravityAndOffset(
+        'Added Success',
+        ToastAndroid.SHORT,
+        ToastAndroid.CENTER,
+        25,
+        50,
+      );
+    }
+  };
+
+  deleteTodo = id => {
+    this.setState(prevState => {
+      const todos = prevState.todos;
+      delete todos[id];
+      const newState = {
+        ...prevState,
+        ...todos,
+      };
+      this.saveTodos(newState.todos); // add this
+      return {...newState};
     });
     ToastAndroid.showWithGravityAndOffset(
-      'One list has finished',
-      ToastAndroid.LONG,
+      'Deleted Success',
+      ToastAndroid.SHORT,
       ToastAndroid.CENTER,
       25,
       50,
     );
-    // console.log(this.state.todos);
   };
 
-  // Delete Todo
-  deleteTodo = id => {
-    axios
-      .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .then(res =>
-        this.setState({
-          todos: [...this.state.todos.filter(todo => todo.id !== id)],
-        }),
-      )
-      .then(() => {
-        ToastAndroid.showWithGravityAndOffset(
-          'Deleted Success',
-          ToastAndroid.LONG,
-          ToastAndroid.CENTER,
-          25,
-          50,
-        );
-      });
-  };
+  markComplete = (id, completed) => {
+    this.setState(prevState => {
+      const newState = {
+        ...prevState,
+        todos: {
+          ...prevState.todos,
+          [id]: {
+            ...prevState.todos[id],
+            completed: completed ? false : true,
+          },
+        },
+      };
+      this.saveTodos(newState.todos); // add this
+      return {...newState};
+    });
 
-  // Add Todo
-  Addtodo = title => {
-    axios
-      .post('https://jsonplaceholder.typicode.com/todos?', {
-        title,
-        completed: false,
-      })
-      .then(res =>
-        this.setState({
-          todos: [...this.state.todos, res.data],
-          newtodo: '',
-        }),
-      )
-      .then(() => {
-        ToastAndroid.showWithGravityAndOffset(
-          'Added Success',
-          ToastAndroid.LONG,
-          ToastAndroid.CENTER,
-          25,
-          50,
-        );
-      });
+    ToastAndroid.showWithGravityAndOffset(
+      completed ? 'One Canceled' : 'One Completed',
+      ToastAndroid.SHORT,
+      ToastAndroid.CENTER,
+      25,
+      50,
+    );
   };
 
   render() {
@@ -117,7 +149,7 @@ class Todo extends Component {
           </Item>
           <Button
             style={styles.ButtonAdd}
-            onPress={() => this.Addtodo(this.state.newtodo)}>
+            onPress={() => this.addTodo(this.state.newtodo)}>
             <Text>Add</Text>
             {/* <Icon type="FontAwesome" name="plus" style={{color: 'white'}} /> */}
           </Button>
